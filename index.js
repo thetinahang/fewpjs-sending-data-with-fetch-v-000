68b630cf4dd418234ba3ca844b50f1f2d2c3e1b8
Tina Hang <mstinahang@gmail.com>
2020-01-11 03:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  let pageBody = document.querySelector("body")
+  let newSection = document.createElement("div")
+
+  return fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+}
